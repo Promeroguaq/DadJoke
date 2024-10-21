9c4b36afa988dad59bcb0a821a0957a17d87a0f3
Promeroguaq <paolaromero829@yahoo.com>
2024-10-20 20:54:11 -0500

    chistes malos

---
+++ main.js
@@ -0,0 +1,15 @@
+const BtnChiste = document.getElementById("BtnChiste") 
+const chistes = document.getElementById("chiste")
+
+BtnChiste.addEventListener("click", generarChiste)
+
+async function generarChiste(){
+    const res = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    const data = res.json()
+    chistes.innerText = data.joke
+    console.log(data)
+}
